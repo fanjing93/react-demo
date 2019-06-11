8c7602f5ede57b557a3753365493bea689e88b10
fanjing <fanjing@wemart.cn>
2019-06-11 20:31:06 +0800

    update

--- demo01/index.jsx
+++ demo01/index.jsx
@@ -18,7 +18,6 @@ ReactDOM.render(element, document.getElementById('root'));*/
 
 setInterval(tick, 1000);*/
 
-
 /*function Clock(props) {
     return (
         <div>
@@ -39,6 +38,7 @@ function tick() {
 
 setInterval(tick, 1000);*/
 
+/*
 class Clock extends React.Component {
     constructor(props) {
         super(props);
@@ -75,17 +75,110 @@ ReactDOM.render(
     <Clock/>,
     document.getElementById('root')
 );
+*/
+
+/*
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+    return (
+        <div>
+            <Welcome name="Sara" />
+            <Welcome name="Cahal" />
+            <Welcome name="Edite" />
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
+*/
+
+/*function formatDate(date) {
+    return date.toLocaleTimeString();
+}
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <div className="UserInfo">
+                <img className="Avatar" src={props.author['avatarUrl']} alt={props.author.name}/>
+                <div className="UserInfo-name">{props.author.name}</div>
+            </div>
+            <div className="Comment-text">{props.text}</div>
+            <div className="Comment-date">{formatDate(props.date)}</div>
+        </div>
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello, Kitty',
+        avatarUrl: 'https://placekitten.com/g/64/64'
+    }
+};
+
+ReactDOM.render(
+    <Comment
+        date={ comment.date}
+        text={comment.text}
+        author={comment.author}/>,
+    document.getElementById('root')
+);*/
 
 
+function Avatar(props) {
+    return (
+        <img className="Avatar" src={props['user']['avatarUrl']} alt={props['user'].name}/>
+    );
+}
 
+function formatDate(date) {
+    return date.toLocaleTimeString();
+}
 
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props['user']}/>
+            <div className="UserInfo-name">{props['user'].name}</div>
+        </div>
+    );
+}
 
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author} />
+            <div className="Comment-text">{props.text}</div>
+            <div className="Comment-date">{formatDate(props.date)}</div>
+        </div>
+    );
+}
 
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React !!!!',
+    author: {
+        name: 'Hello, Kitty',
+        avatarUrl: 'https://placekitten.com/g/64/64'
+    }
+};
 
-
-
-
-
+ReactDOM.render(
+    <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}/>,
+    document.getElementById('root')
+);
 
 
 
